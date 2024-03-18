67b1e1ee6bef0a663cc5d61cce59393868a30e07
som141 <163431679+som141@users.noreply.github.com>
2024-03-18 22:32:32 +0900

    "sdf"

--- src/test/java/hello/hello/Spring/service/serviceTest.java
+++ src/test/java/hello/hello/Spring/service/serviceTest.java
@@ -36,7 +36,7 @@ class serviceTest {
 
     }
     @Test
-    public void 중복제거(){
+    public void 중복제거임(){
         member m1= new member();
         m1.setName("1");
         member m2= new member();
